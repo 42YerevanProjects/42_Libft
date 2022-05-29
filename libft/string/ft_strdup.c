d765da12af146857c7d205f6696786ea1a171a4d
Nathanael <sargishovsepyan2001@gmail.com>
2022-05-29 07:01:26 -0400

    Functions working with strings

---
+++ libft/string/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/28 20:49:06 by shovsepy          #+#    #+#             */
+/*   Updated: 2021/01/28 20:53:41 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*start;
+	int		i;
+	int		len;
+
+	len = ft_strlen(s1);
+	if (!(start = malloc(len + 1)))
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		start[i] = s1[i];
+		i++;
+	}
+	start[i] = 0;
+	return (start);
+}
